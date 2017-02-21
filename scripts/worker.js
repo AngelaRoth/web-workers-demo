34d6d6f3fdba4871faca9a5becf67313e481748e
Angela <Angela>
2017-02-21 16:47:00 -0500

    re-add .data to imageData in manipulation loop

--- scripts/worker.js
+++ scripts/worker.js
@@ -6,17 +6,17 @@ this.onmessage = function(e) {
   var type = e.data.type;
 
   try {
-    length = imageData.length / 4;
+    length = imageData.data.length / 4;
     for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData[i * 4 + 0];
-      g = imageData[i * 4 + 1];
-      b = imageData[i * 4 + 2];
-      a = imageData[i * 4 + 3];
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
       pixel = manipulate(type, r, g, b, a);
-      imageData[i * 4 + 0] = pixel[0];
-      imageData[i * 4 + 1] = pixel[1];
-      imageData[i * 4 + 2] = pixel[2];
-      imageData[i * 4 + 3] = pixel[3];
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
     }
     postMessage(imageData);
   } catch (e) {
